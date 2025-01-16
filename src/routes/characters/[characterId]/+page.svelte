ec65815e88cf4e91b190e77f272476bcea306666
systemnotmysister <48369226+systemnotmysister@users.noreply.github.com>
2025-01-16 15:06:54 +0200

    commit

--- src/routes/characters/[characterId]/+page.svelte
+++ src/routes/characters/[characterId]/+page.svelte
@@ -1,85 +1,44 @@
 <script lang="ts">
-  import { client } from '$lib/graphqlClient';
-  import { page } from '$app/state';
+  import { page } from "$app/state";
+  import {
+    GetCharacterDocument,
+    type GetCharacterQuery,
+    type GetCharacterQueryVariables,
+  } from "$lib/graphql/generated/graphql";
+  import { getContextClient, queryStore } from "@urql/svelte";
 
-  interface Episode {
-    id: string;
-    name: string;
-    episode: string;
-  }
+  const characterId = page.params.characterId;
 
-  interface Character {
-    id: number;
-    name: string;
-    status: string;
-    species: string;
-    gender: string;
-    image: string;
-    episode: Episode[];
-  }
-
-  let character: Character | null = null;
-
-  // Запрос для получения героя и эпизодов
-  const query = `
-    query($id: ID!) {
-      character(id: $id) {
-        id
-        name
-        status
-        species
-        gender
-        image
-        episode {
-          id
-          name
-          episode
-        }
-      }
-    }
-  `;
-
-  const fetchCharacter = async (id: number) => {
-    try {
-      const { data } = await client.query(query, { id }).toPromise();
-      if (data && data.character) {
-        character = data.character;
-      } else {
-        console.error('Нет данных о персонаже');
-      }
-    } catch (error) {
-      console.error('Ошибка при получении данных:', error);
-    }
-  };
-
-  $: {
-    const characterId = page.params.characterId;
-    if (characterId) {
-      fetchCharacter(parseInt(characterId));
-    }
-  }
+  const store = queryStore<GetCharacterQuery, GetCharacterQueryVariables>({
+    client: getContextClient(),
+    query: GetCharacterDocument,
+    variables: {
+      id: characterId,
+    },
+  });
 </script>
 
-<h1>{character ? character.name : 'Загрузка...'}</h1>
-
-{#if character}
+{#if $store.data?.character}
   <div>
-    <img src={character.image} alt={character.name} />
-    <p>Age: {character.status}</p>
-    <p>Species: {character.species}</p>
-    <p>Gender: {character.gender}</p>
+    <img src={$store.data.character.image} alt={$store.data.character.name} />
+    <p>Status: {$store.data.character.status}</p>
+    <p>Species: {$store.data.character.species}</p>
+    <p>Gender: {$store.data.character.gender}</p>
 
-    <h2>Took part in:</h2>
+    <h2>Episodes:</h2>
     <ul>
-      {#each character.episode as episode}
+      {#each $store.data.character.episode as episode (episode?.id)}
         <li>
-          <a href={`/season/${episode.episode.slice(1, 3)}/${episode.episode.slice(4, 6)}`}>
-            {episode.name} ({episode.episode})
+          <a
+            href={`/season/${episode?.episode?.slice(1, 3)}/${episode?.episode?.slice(4, 6)}`}
+          >
+            {episode?.name || "Без названия"} ({episode?.episode ||
+              "Неизвестный эпизод"})
           </a>
         </li>
       {/each}
     </ul>
   </div>
 {:else}
-  <p>No character data</p>
+  <p>Нет данных о персонаже</p>
 {/if}
